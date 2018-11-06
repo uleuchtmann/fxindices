dfef3f563e10a31fd086e2ae95b8c94d63d4d934
Ulrich Leuchtmann <ulrich.leuchtmann@t-online.de>
2018-11-06 09:23:18 +0100

    read in EURUSD in main.R

--- main.R
+++ main.R
@@ -1,2 +1,11 @@
 # main file as working horse for the project: testing, development etc.
 
+# read in EURUSD data
+options(blpAutoConnect=TRUE, blpHost="192.168.2.130", blpPort=18194L)
+library(Rblpapi)
+blpConnect()
+library(timeSeries)
+library(purrr)
+source("RblpapiExtra.R")
+bbfix2timeSeries("EURUSD", as.Date("2017-01-01"), as.Date("2017-12-31"))
+
