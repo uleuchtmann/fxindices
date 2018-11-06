8a2c6d9944b65806de56da4de081e42aaef5a6ac
Ulrich Leuchtmann <ulrich.leuchtmann@t-online.de>
2018-11-07 00:40:04 +0100

    preparation for fBm analysis of EURUSD

--- main.R
+++ main.R
@@ -7,9 +7,26 @@ con <- blpConnect()
 library(timeSeries)
 library(purrr)
 source("RblpapiExtra.R")
-EURUSD <- bbfix2timeSeries("EURUSD", as.Date("2007-03-01"), as.Date("2018-11-05"))
+EURUSD <- bbfix2timeSeries("EURUSD", as.Date("2007-02-28"), as.Date("2018-11-05"))
+setFinCenter(EURUSD) <- "GMT"
 plot(EURUSD)
-e <- log(EURUSD)
 
 
-
+# transformations
+# 30-minutes data
+e30  <- window(log(EURUSD), "2007-03-01 00:00:00", "2018-10-31 23:30:00") 
+De30 <- window(100.0 * diff(log(EURUSD)), 
+               "2007-03-01 00:00:00", "2018-10-31 23:30:00") 
+Ve30 <- window((100.0*filter(log(EURUSD), 
+                             c(1,-2,1), method="convolution", sides=1))^2.0,
+               "2007-03-01 00:00:00", "2018-10-31 23:30:00") 
+# 60-minutes data 
+e60  <- window(log(EURUSD[seq(2,length(EURUSD),2), ]), 
+               "2007-03-01 00:00:00", "2018-10-31 23:30:00")
+De60 <- window(100.0 * diff(log(EURUSD[seq(2,length(EURUSD),2), ])), 
+               "2007-03-01 00:00:00", "2018-10-31 23:30:00")
+Ve60 <- window((100.0*filter(log(EURUSD[seq(2,length(EURUSD),2), ]), 
+                             c(1,-2,1), method="convolution", sides=1))^2.0, 
+               "2007-03-01 00:00:00", "2018-10-31 23:30:00")
+  
+  
