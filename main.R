6c3aa964c7fda0ba1605991c63b39d520a112931
Ulrich Leuchtmann <ulrich.leuchtmann@t-online.de>
2018-11-06 10:29:10 +0100

    read in EURUSD - improved: full range, plot, log transformation

--- main.R
+++ main.R
@@ -3,9 +3,13 @@
 # read in EURUSD data
 options(blpAutoConnect=TRUE, blpHost="192.168.2.130", blpPort=18194L)
 library(Rblpapi)
-blpConnect()
+con <- blpConnect()
 library(timeSeries)
 library(purrr)
 source("RblpapiExtra.R")
-bbfix2timeSeries("EURUSD", as.Date("2017-01-01"), as.Date("2017-12-31"))
+EURUSD <- bbfix2timeSeries("EURUSD", as.Date("2007-03-01"), as.Date("2018-11-05"))
+plot(EURUSD)
+e <- log(EURUSD)
+
+
 
