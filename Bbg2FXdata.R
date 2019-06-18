f494a55b25fcbddee0536f644de3f52bef361709
Ulrich Leuchtmann <ulrich.leuchtmann@t-online.de>
2019-06-18 14:43:07 +0200

    chgedanged end date for reading data in to Sys.Date().

--- Bbg2FXdata.R
+++ Bbg2FXdata.R
@@ -1,13 +1,13 @@
 # read in Bloomberg FX fixing data and 
 # store as timeSeries object in file FX.RData
 
-# assumes Bloomberg runs on 192.168.2.124
+# assumes Bloomberg runs on 192.168.2.116
 # assumes on that machine the following commands have been executed:
 #   start C:\blp\DAPI\bbcomm.exe
 #   socat TCP4-LISTEN:18194,fork TCP4:localhost:8194
-# options(blpAutoConnect=TRUE, blpHost="192.168.2.124", blpPort=18194L)
+options(blpAutoConnect=TRUE, blpHost="192.168.2.116", blpPort=18194L)
 # if Bloomberg runs on localhost, use instead
-options(blpAutoConnect=TRUE)
+# options(blpAutoConnect=TRUE)
 
 library(Rblpapi)
 library(timeSeries)
@@ -24,6 +24,6 @@ currencies <- c("EURUSD", "USDJPY", "GBPUSD", "USDCHF", "USDCAD", "AUDUSD",
                 "USDBRL", "USDCLP")
 
 # shifted end date to "2019-05-28"
-FX <- bbfix2timeSeries(currencies, as.Date("2007-02-28"), as.Date("2019-05-28"))
+FX <- bbfix2timeSeries(currencies, as.Date("2007-02-28"), as.Date(Sys.Date()))
 
 save(FX, file="FX.RData")
